66beef217b60cf267b92471e5cdcf1e0b7104dea
MatheusEssu <matheus.essu@sptech.school>
2024-05-24 03:29:27 -0300

    Implementação da API web-data-viz V2 (Cadastro está funcionando)

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,54 +6,35 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+create database ProjetoIndividual;
 
-USE aquatech;
+use ProjetoIndividual;
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
-);
+create table usuario (
+idUsuario int primary key auto_increment,
+nome varchar(60),
+email varchar(50),
+senha varchar(20),
+fkIdolo int,
+foreign key (fkIdolo) references idolo (idIdolo));
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+create table idolo (
+idIdolo int primary key auto_increment,
+nomeIdolo varchar(50),
+nacionalidade varchar(40),
+periodo varchar(45));
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+create table quiz (
+idQuiz int primary key auto_increment,
+tentativas int,
+corretas int,
+erradas int,
+pontuacao varchar(45),
+fkUsuario int,
+foreign key (fkUsuario) references usuario (idUsuario)); 
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+insert into idolo values
+(null, 'Cássio', 'Brasil', '2012-2024'),
+(null, 'Sócrates', 'Brasil', '1978-1984'),
+(null, 'Marcelinho Carioca', 'Brasil', '1994-2001'),
+(null, 'Neto', 'Brasil', '1989-1993 e 1996-1997');
